674c5eafd077739e97e3bc3b1837d83177270036
lostinserver <patrioticprerit@gmail.com>
2025-02-10 05:27:18 +0545

    [Updated] project

--- src/store/provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React, { ReactNode } from "react";
-import { Provider } from "react-redux";
-import { store } from ".";
-
-export const ReduxProvider = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
