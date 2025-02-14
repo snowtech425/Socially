d82ccea55bb0bb56f35d5addfe21d0740d1c6a7e
lostinserver <patrioticprerit@gmail.com>
2025-02-14 06:45:38 +0545

    [Updated] hume config

---
+++ frontend/src/app/Hume/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import dynamic from "next/dynamic";
+import { getHumeAccessToken } from "../../lib/getHumeAccessToken";
+import { HumeClient } from "hume";
+
+const Chat = dynamic(() => import("../../components/Chat"), {
+  ssr: false,
+});
+
+const client = new HumeClient(getHumeAccessToken);
+
+const socket = await client.empathicVoice.chat.connect({
+  configId: String(process.env.NEXT_PUBLIC_HUME_CONFIG_ID1),
+});
+
+export default async function HumePage() {
+  const accessToken = await getHumeAccessToken();
+
+  if (!accessToken) {
+    throw new Error();
+  }
+
+  return (
+    <div className={"grow flex flex-col"}>
+      <Chat accessToken={accessToken} />
+    </div>
+  );
+}
--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardContent } from "./ui/card";
+import { FaCoffee, FaHeartbeat } from "react-icons/fa";
 
 interface ModalProps {
   isOpen: boolean;
@@ -36,10 +37,14 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
           {[
             {
               title: "Date",
+              icon: <FaHeartbeat />,
+              color: "text-red-500",
               description: "Make your Date Perfect Before your Actual Date",
             },
             {
               title: "Barista",
+              icon: <FaCoffee />,
+              color: "text-red-900",
               description: "Learn How to Interact as a Professional Barista",
             },
           ].map((item, index) => (
@@ -48,8 +53,11 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
               className="bg-gradient-to-br from-blue-100 via-green-100 to-pink-100 dark:from-green-800 dark:via-green-800 dark:to-green-800 rounded-xl p-6 md:p-10 shadow-lg hover:shadow-2xl transition-shadow"
             >
               <CardContent className="cursor-pointer">
-                <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">
-                  {item.title}
+                <h3 className="text-lg font-semibold  mb-2 flex items-center gap-x-3">
+                  <span className={item.color}> {item.icon}</span>
+                  <span className="text-gray-900 dark:text-white">
+                    {item.title}
+                  </span>
                 </h3>
                 <p className="text-sm text-gray-700 dark:text-gray-300">
                   {item.description}
