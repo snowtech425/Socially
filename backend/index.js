27692cf5e8a49f46f736d8f1aaa270735ddbec4a
prathamshrestha <prathamshrestha358@gmail.com>
2025-02-23 18:07:36 -0500

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
 
 app.use(
